13b8d141241b72fb4c0be0191ca7c734f98f6ecf
denlog1977 <denlog1977@gmail.com>
2019-11-04 01:55:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a331appresources_portrait_lanscape/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a331appresources_portrait_lanscape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
